34e6dfe4840acff79bfd64105a6070291ab4cacd
MzALI134 <mueezaliazrar@gmail.com>
2025-02-11 15:20:55 +0000

    Clean repository

--- middleware/error.js
+++
@@ -1,6 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  res.status(500).json({
-    status: 'error',
-    message: 'Server error'
-  })
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-            <h1>WebLink API</h1>
-            <p>Access your player data at: /api/YOUR_KEY</p>
-        </div>
-    )
-}
