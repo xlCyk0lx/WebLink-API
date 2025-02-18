8672b878920d00f4543c94701c1a442c0c99bf9e
MzALI134 <mueezaliazrar@gmail.com>
2025-02-18 21:21:36 +0000

    Database update

--- public/server.js
+++
@@ -1,8 +0,0 @@
-// Add these routes
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/dashboard.html'));
-});
-
-app.get('/help/weblink', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/help/weblink.html'));
-});
